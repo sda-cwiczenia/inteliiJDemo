94c6882741841afcb69ce0399b3122198cd17493
Irek Piatkowski <irekp@onet.pl>
2019-04-18 00:59:52 +0200

    3-rd line added

--- src/com/ipiatkow/Main.java
+++ src/com/ipiatkow/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello Ireneusz");
+        System.out.println("Hello Ireneusz 3rd time");
     }
 }
