259582f82025bc1e5ce81ed05722f615318654d8
Irek Piatkowski <irekp@onet.pl>
2019-04-17 21:42:16 +0200

    Adding hello sout

--- src/com/ipiatkow/Main.java
+++ src/com/ipiatkow/Main.java
@@ -3,6 +3,6 @@ package com.ipiatkow;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hellow world!");
+        System.out.println("Hello world!");
     }
 }
